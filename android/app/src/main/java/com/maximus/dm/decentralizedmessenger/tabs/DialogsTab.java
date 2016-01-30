aa0a28339a15630f340db434087892172242afe4
Maximus <lizuram@tcd.ie>
2016-01-30 22:16:36 +0000

    android listview added to dialogs tab

--- android/app/src/main/java/com/maximus/dm/decentralizedmessenger/tabs/DialogsTab.java
+++ android/app/src/main/java/com/maximus/dm/decentralizedmessenger/tabs/DialogsTab.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.maximus.dm.decentralizedmessenger.R;
 
@@ -16,11 +18,28 @@ public class DialogsTab extends Fragment {
 
     public static String TAB_NAME = "Dialogs";
 
+    ListView dialogList;
+
     //set view for the fragment
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_dialogs, container, false);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View v = this.getView();
+
+        String[] listData = {"shoutout", "to", "whoeverz", "readn", "this", "you're beautiful"};
+        ArrayAdapter<String> dialogAdapter = new ArrayAdapter<String>(
+                getContext(), R.layout.listelement_dialog, R.id.tvListString, listData);
+
+        dialogList = (ListView) v.findViewById(R.id.list_dialog);
+        dialogList.setAdapter(dialogAdapter);
+    }
+
+
 
 }
