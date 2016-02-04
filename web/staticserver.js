13ccbf3c4012ba5fbf62a2ce2c497a3842273889
John Kelly <kellyj58@tcd.ie>
2016-02-04 23:23:51 +0000

    endpoint development stuff

--- web/staticserver.js
+++ web/staticserver.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static("www"));
 app.listen(80);
 var id = 0;
+var messageId = 0;
 var bp = require("body-parser");
 app.use(bp.urlencoded({extended: true}));
 var messages = [];
@@ -18,10 +19,19 @@ app.get("/secret", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
+  console.log("hit");
   users[req.body.user] = id;
   res.send({success: true, token: "sample token", id: id++ })
 });
 
 app.post("/login", function(req, res) {
   res.send({success: true, token: "sample token", id: users[req.body.user] });
+});
+
+app.post("/message", function(req, res) {
+  messages.push({ message: req.body.message, id: messageId++ })
+})
+
+app.post("/messages", function(req, res) {
+
 });
