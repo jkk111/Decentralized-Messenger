c61bd03ca0d34fca7289cadaa68feceb5d9bf722
John Kelly <kellyj58@tcd.ie>
2016-03-30 20:29:15 +0100

    Removed cluster, rewrote to act as single thread, instead of thread master

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
  * Imports necessary modules, starts the admin panel,
  * Then creates multiple forks of the process to run the application
  */
-module.exports = function(cluster) {
+module.exports = function(cluster, logger) {
   var fs = require("fs");
   var conf;
   try {
@@ -16,7 +16,7 @@ module.exports = function(cluster) {
   var d = new Date();
   var express = require("express");
   var app = express();
-  var logger = require("./logger.js")(process.pid, process.env.StartTime || d, true);
+  // var logger = require("./logger.js")(process.pid, process.env.StartTime || d, true);
   var bodyParser = require("body-parser");
   var cookieParser = require("cookie-parser");
   app.use(logger.route);
@@ -29,39 +29,18 @@ module.exports = function(cluster) {
     });
     next();
   });
-
   app.use(cookieParser());
-  app.use(express.static("../web/www"));
+  app.use(express.static(__dirname + "../web/www"));
 
-  var ct = require("./crosstalk.js")(conf);
-  const numCPUs = conf.threads || require('os').cpus().length;
-
-  // Start a thread for each cpu core
-  if(cluster.isMaster) {
-    var admin = require("./admin.js")(conf, false);
-    app.use("/admin", admin.route);
-    for(var i = 0 ; i < numCPUs; i++) {
-      var worker = cluster.fork({StartTime: d});
-      worker.on("message", function(data) {
-        logHandler(data, admin);
-      });
-    }
-    cluster.on("exit", function(worker, code, signal) {
-      console.log("a worker died, restarting....");
-      var worker = cluster.fork({StartTime: d});
-      worker.on("message", function(data) {
-        logHandler(data, admin);
-      })
-    });
-
-    logger.addListener(function(data) {
-      logHandler(data, admin);
-    })
-  } else {
-    logger.addListener(masterListener);
-    var storage = require("./storage.js")(conf);
-    var messaging = require("./messaging.js")(app, storage, ct, conf);
-  }
+  var ct = require(__dirname +"/crosstalk.js")(conf);
+  var admin = require(__dirname + "/admin.js")(conf, false);
+  app.use("/admin", admin.route);
+  logger.addListener(function(data) {
+    logHandler(data, admin);
+  });
+  logger.addListener(masterListener);
+  var storage = require("./storage.js")(conf);
+  var messaging = require("./messaging.js")(app, storage, ct, conf);
 
   function masterListener(key, data) {
     var el = {}
